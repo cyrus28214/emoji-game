59a697e0c7a7b2131499514c250e4e479eb970da
Cyrus28214 <1270302477@qq.com>
2023-10-04 11:54:19 +0800

    Add game.js
    Initialize canvas and ctx variables in game.js
    
    Add main function in game.js, listen DOMContentLoaded to call main

---
+++ game.js
@@ -0,0 +1,12 @@
+let canvas;
+let ctx;
+
+function main() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
+
+
