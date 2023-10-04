f32d917bf3ad0ef88ab217f5a516e90dfa00c882
Cyrus28214 <1270302477@qq.com>
2023-10-04 13:40:52 +0800

    Add 4 image assets to ./images
    In game.js:
        Add loadImage()
        Add images{}
        Add Player class
        Implement Player.update() to render this.image
        Add and Implement gameLoop()
        Update main():
            Set canvas size
            Call gameLoop()

--- game.js
+++ game.js
@@ -1,10 +1,59 @@
 let canvas;
 let ctx;
 
+let WIDTH = 1600;
+let HEIGHT = 1200;
+
+function loadImage(path) {
+    let image = document.createElement("img");
+    image.src = path;
+    return image;
+}
+
+let images = {
+    turtle: loadImage("./images/turtle.png"),
+};
+
+class Player {
+    constructor() {
+        this.x = WIDTH/2;
+        this.y = HEIGHT/2;
+        this.pivot_x = 0.5;
+        this.pivot_y = 0.5;
+        this.image = images.turtle;
+        this.width = 200;
+        this.height = 200;
+    }
+
+    update() {
+        if (this.image.complete) {
+            ctx.drawImage(
+                this.image,
+                this.x - this.width * this.pivot_x,
+                this.y - this.height * this.pivot_y,
+                this.width,
+                this.height
+            );
+        }
+    }
+}
+
+let player = new Player();
+
 function main() {
     canvas = document.getElementById("canvas");
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
     ctx = canvas.getContext("2d");
+    gameLoop();
 }
+
+function gameLoop() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    player.update();
+    requestAnimationFrame(gameLoop);
+}
+
 
 document.addEventListener("DOMContentLoaded", main);
 
